20e3b29310da15c3c6c0ed7c3714a50334c0b861
KranthiReddyVitta <kranthi.reddy@publicissapient.com>
2023-03-22 16:14:03 +0530

    Added Dependecny

--- application/templates/Pokemon.js
+++ application/templates/Pokemon.js
@@ -15,6 +15,9 @@ const Pokemon = ({ types }) => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state);
   const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonUrl, setPokemonUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
   const { genders, males, females, genderless } = useSelector(
     (state) => state.pokemon
   );
@@ -37,9 +40,10 @@ const Pokemon = ({ types }) => {
 
   const getPokemons = useCallback(() => {
     agent.pokemonApis.allPokemon().then((data) => {
+      setPokemonUrl(data.next);
       getPokemonDetails(data.results);
     });
-  }, []);
+  }, [pokemonUrl]);
 
   /**
    * @method To Fetch Individual PokemonData
